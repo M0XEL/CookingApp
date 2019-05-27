82b9e68e5693939f8e42874db720ea5d054b98bb
M0XEL <moxelyo@gmail.com>
2019-05-27 18:57:55 +0200

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,11 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TrendingPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'CookingApp',
+    theme: ThemeData(primarySwatch: Colors.blue),
+    home: TrendingPage(),
+  );
 }
 
 
