b1c2de767de9e2a8742f90581a26d3ab1dbb6403
M0XEL <moxelyo@gmail.com>
2019-06-02 10:39:51 +0200

    add Firestore

--- lib/ShoppingPage.dart
+++ lib/ShoppingPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'MyBottomNavigationBar.dart';
 
 class ShoppingPage extends StatefulWidget {
@@ -9,7 +10,26 @@ class ShoppingPage extends StatefulWidget {
 class _ShoppingPageState extends State<ShoppingPage> {
   @override
   Widget build(BuildContext context) => Scaffold(
-    body: Text('Einkaufsliste'),
     bottomNavigationBar: MyBottomNavigationBar(index: 1),
+    body: StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance.collection('Rezepte').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) return LinearProgressIndicator();
+
+        var list = snapshot.data.documents;
+        return ListView(
+          children: list.map((data) => _buildListTile(data)).toList(),
+        );
+      },
+    ),
+  );
+
+  Widget _buildListTile(DocumentSnapshot data) => ListTile(
+    leading: FlutterLogo(),
+    title: Text(data['Name']),
+    trailing: IconButton(
+      icon: Icon(Icons.check),
+      onPressed: null,
+    ),
   );
 }
