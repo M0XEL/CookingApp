1d287e06c94c736c29d196d0c9190867d4de2be3
M0XEL <moxelyo@gmail.com>
2019-07-16 15:23:54 +0200

    change RecipePage

--- lib/RecipePage.dart
+++ lib/RecipePage.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' as prefix0;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'MyDrawer.dart';
 
 class RecipePage extends StatefulWidget {
@@ -11,17 +14,42 @@ class RecipePage extends StatefulWidget {
   _RecipePageState createState() => _RecipePageState(documentId);
 }
 
+class Ingredient {
+  String name;
+  int amount;
+}
+
 class _RecipePageState extends State<RecipePage> {
   final String recipeId;
-  _RecipePageState(this.recipeId);
+  String recipeName;
+  List<Ingredient> ingredients;
+
+  _RecipePageState(this.recipeId) {
+    Firestore.instance.collection('recipes').document(recipeId).get().then((document) {
+      recipeName = document['name'];
+    });
+    Firestore.instance.collection('recipes').document(recipeId).collection('ingredients').getDocuments().then((documents) {
+      documents.documents.forEach((document) {
+        Ingredient ingredient;
+        ingredient.amount = document.data['amount'];
+        String id = document.data['id'];
+        Firestore.instance.collection('ingredients').document(id).get().then((document) {
+          ingredient.name = document['name'];
+          ingredients.add(ingredient);
+        });
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
     drawer: MyDrawer(),
-    body: Stack(children: <Widget>[
-    NestedScrollView(
-      headerSliverBuilder: (BuildContext context, bool a) {
-        return <Widget>[
+    body: Column(
+        children: <Widget>[
+          Expanded(
+            child:
+          CustomScrollView(
+        slivers: <Widget>[
           SliverAppBar(
             expandedHeight: 195.0,
             pinned: true,
@@ -48,11 +76,9 @@ class _RecipePageState extends State<RecipePage> {
               background: Image.asset('images/pizza.jpg'),
             ),
           ),
-        ];
-      },
-      body: Column(
-        children: <Widget>[
-          ButtonBar(
+          SliverList(
+            delegate: SliverChildListDelegate([
+              ButtonBar(
             alignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               IconButton(
@@ -68,34 +94,52 @@ class _RecipePageState extends State<RecipePage> {
                 onPressed: null,
               ),
             ],
-          )
+          ),
+            ]),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            delegate: SliverChildBuilderDelegate((BuildContext context, int index) {
+              if (index.isEven) {
+                return Container(
+                  child: Text('100g'),
+                );
+              } else {
+                return Container(
+                  child: Text('Teig'),
+                );
+              }
+            },
+            childCount: 4
+            )//ingredients.length)
+          ),
         ],
-      ),
-    ),
-    Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(child: Container()),
-        Card(
-          margin: EdgeInsets.all(0.0),
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
-          color: Colors.lightGreen,
-          child: MaterialButton(
-            height: 56.0,
-            child: Text('START COOKING',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 1.0,
-                fontWeight: FontWeight.bold,
-                fontSize: 16.0,
+      ),),
+      Row(
+        children: <Widget>[
+          Expanded(
+            child: Card(
+              margin: EdgeInsets.all(0.0),
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+              color: Colors.lightGreen,
+              child: MaterialButton(
+                height: 56.0,
+                child: Text('START COOKING',
+                  style: TextStyle(
+                    color: Colors.white,
+                    letterSpacing: 1.0,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                onPressed: null,
               ),
             ),
-            onPressed: null,
           ),
-        ),
-      ],
-    )
-    ],
-    ),
+        ],
+      ),
+      
+        ],
+      )
   );
 }
