085fd07b9d6861147ede7e077fff7b8adbeba0ef
M0XEL <moxelyo@gmail.com>
2019-07-28 10:04:08 +0200

    add preparation list and new Card style

--- lib/RecipePage.dart
+++ lib/RecipePage.dart
@@ -1,9 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart' as prefix0;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
 import 'MyDrawer.dart';
 
@@ -24,7 +22,7 @@ class _RecipePageState extends State<RecipePage> {
   final String recipeId;
   String recipeName = '';
   List<Ingredient> ingredients = List<Ingredient>();
-  Future<List<Ingredient>> i;
+  List<String> steps = List<String>();
 
   _RecipePageState(this.recipeId) {
     Firestore.instance.collection('recipes').document(recipeId).get().then((document) {
@@ -35,15 +33,67 @@ class _RecipePageState extends State<RecipePage> {
   buildIngredientColumn() {
     List<Widget> ingredientList = List<Widget>();
     ingredients.forEach((ingredient) {
-      ingredientList.add(Row(
-        children: <Widget>[
-          Text(ingredient.amount.toString()),
-          Text(ingredient.name),
-        ],
-      ));
+      ingredientList.add(
+        Row(
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.only(right: 8.0),
+              width: MediaQuery.of(context).size.width * 0.4,
+              child: Text(ingredient.amount.toString(),
+                textAlign: TextAlign.right,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(left: 8.0),
+                child: Text(ingredient.name,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+      ingredientList.add(Container(height: 8.0));
+      //ingredientList.add(Divider());
     });
     return Column(
       children: ingredientList,
+    );
+  }
+
+  buildSteps() {
+    List<Widget> stepsList = List<Widget>();
+    for (int i = 0; i < steps.length; i++) {
+      stepsList.add(
+        ListTile(
+          leading: Container(
+            width: 32.0,
+            height: 32.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.orange,
+            ),
+            child: Center(
+              child: Text(i.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          title: Text(steps[i]),
+          //subtitle: Container(height: 8.0),
+        )
+      );
+    }
+    return Column(
+      children: stepsList,
     );
   }
 
@@ -57,6 +107,7 @@ class _RecipePageState extends State<RecipePage> {
             future: Future.wait([
               Firestore.instance.collection('recipes').document(recipeId).collection('ingredients').getDocuments(),
               Firestore.instance.collection('ingredients').getDocuments(),
+              Firestore.instance.collection('recipes').getDocuments(),
             ]),
             builder: (BuildContext context, AsyncSnapshot snapshot) {
               if (!snapshot.hasData) {
@@ -74,6 +125,10 @@ class _RecipePageState extends State<RecipePage> {
                     ingredient.name = d.data['name'];
                     ingredients.add(ingredient);
                   });
+                  var d5 = snapshot.data[2].documents.firstWhere((d4) {
+                    return d4.documentID == recipeId;
+                  });
+                  steps = d5['steps'].cast<String>();
                   return CustomScrollView(
                     slivers: <Widget>[
                       SliverAppBar(
@@ -104,24 +159,84 @@ class _RecipePageState extends State<RecipePage> {
                       ),
                       SliverList(
                         delegate: SliverChildListDelegate([
-                          ButtonBar(
-                            alignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              IconButton(
-                                icon: Icon(Icons.book),
-                                onPressed: null,
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.add_shopping_cart),
-                                onPressed: null,
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.calendar_today),
-                                onPressed: null,
-                              ),
-                            ],
+                          Card(
+                            child: ButtonBar(
+                              alignment: MainAxisAlignment.spaceEvenly,
+                              children: <Widget>[
+                                IconButton(
+                                  icon: Icon(Icons.book),
+                                  onPressed: null,
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.add_shopping_cart),
+                                  onPressed: null,
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.calendar_today),
+                                  onPressed: null,
+                                ),
+                              ],
+                            ),
                           ),
-                          buildIngredientColumn(),
+                          Card(
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Container(width: 40.0),
+                                  title: Text('Ingredients',
+                                    style: TextStyle(
+                                      fontSize: 24.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 1.4,
+                                      color: Colors.orangeAccent,
+                                    ),
+                                  ),
+                                ),
+                                buildIngredientColumn(),
+                                ListTile(
+                                  leading: Container(width: 40),
+                                  title: Row(
+                                    children: <Widget>[
+                                      Text('for'),
+                                      Text(' - 2 + '),
+                                      Text('servings'),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Card(
+                            child: ListTile(
+                              leading: Container(width: 40.0),
+                              title: Text('Nutrition Info',
+                                style: TextStyle(
+                                  fontSize: 24.0,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 1.4,
+                                  color: Colors.orangeAccent,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Card(
+                            child: Column(
+                              children: <Widget>[
+                                ListTile(
+                                  leading: Container(width: 40.0),
+                                  title: Text('Preparation',
+                                    style: TextStyle(
+                                      fontSize: 24.0,
+                                      fontWeight: FontWeight.bold,
+                                      letterSpacing: 1.4,
+                                      color: Colors.orangeAccent,
+                                    ),
+                                  ),
+                                ),
+                                buildSteps(),
+                              ],
+                            ),
+                          ),
                         ]),
                       ),
                     ],
